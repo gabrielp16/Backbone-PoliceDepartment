99a0e2f54322024af44d458db35186c79dde1495
Gabriel Peña <gabriel.p16@gmail.com>
2020-08-25 00:06:39 -0500

    Puesta en produccion en GitHub

---
+++ docs/precache-manifest.bcbf044ec18b0f735714fa771090bcd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adf7a1ff7b22714d941f6f738f7b110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0c1aac9b026ccdc41ea",
+    "url": "/static/css/main.befd6a39.chunk.css"
+  },
+  {
+    "revision": "60483a4e00a2e0891663",
+    "url": "/static/js/2.bd02f5ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd02f5ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c1aac9b026ccdc41ea",
+    "url": "/static/js/main.cffcffa8.chunk.js"
+  },
+  {
+    "revision": "7cc84ea2cdda183e0e8d",
+    "url": "/static/js/runtime-main.ab9ecd31.js"
+  },
+  {
+    "revision": "71bbec9d96561e7ad83bfa93fc73ff80",
+    "url": "/static/media/bike.71bbec9d.png"
+  },
+  {
+    "revision": "a3f3059987d64facba87f7d21845e96c",
+    "url": "/static/media/logo-app.a3f30599.png"
+  }
+]);
